210987eb28a4997479a453bd4c551098bd3e49d5
Quantcom SEA <ckkacc@gmail.com>
2022-02-12 14:29:36 +0800

    update natswrapper to faciliate a push to Digital Ocean cluster

--- orders/src/nats-wrapper.ts
+++ orders/src/nats-wrapper.ts
@@ -19,7 +19,7 @@ class NatsWrapper {
 
         return new Promise<void>((resolve, reject) => {
             this.client.on('connect', () => {
-                console.log('Connected to NATS!!!!!!!!!!!');
+                console.log('Connected to NATS!!!!!!!!!!!!');
                 resolve();
             });
             this.client.on('error', (err) => {
